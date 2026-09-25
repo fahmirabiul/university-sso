1a10f49ac4a6008fc7af3ada9124aaa7dae4c3f8
fahmirabiul <fahmirk@gmail.com>
2026-09-25 22:07:44 +0700

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,11 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use App\Models\UserProfile;
+use App\Models\Role;
+use App\Models\StudyProgram;
+use App\Models\Unit;
 use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,12 +22,17 @@ class DatabaseSeeder extends Seeder
             MasterDataSeeder::class,
         ]);
 
-        $roles = \App\Models\Role::all()->keyBy('name');
-        $studyPrograms = \App\Models\StudyProgram::all()->keyBy('name');
-        $units = \App\Models\Unit::all()->keyBy('name');
+        $roles = Role::all()->keyBy('name');
+        $studyPrograms = StudyProgram::all()->keyBy('name');
+        $units = Unit::all()->keyBy('name');
+
+        $faker = Faker::create('id_ID');
 
         // 1. Super Admin
-        $admin = User::factory()->create(['email' => 'admin@university.ac.id']);
+        $admin = User::factory()->create([
+            'id' => '11111111-1111-1111-1111-100000000001',
+            'email' => 'admin@university.ac.id'
+        ]);
         UserProfile::create([
             'user_id' => $admin->id,
             'identifier_number' => 'ADM001',
@@ -32,46 +41,51 @@ class DatabaseSeeder extends Seeder
         ]);
         if (isset($roles['admin'])) $admin->roles()->attach($roles['admin']->id);
 
-        // 2. Admin Unit (5 Users)
-        for ($i = 1; $i <= 5; $i++) {
-            $user = User::factory()->create(['email' => "admin.unit{$i}@university.ac.id"]);
+        // 2. Admin Unit (4 Users)
+        for ($i = 1; $i <= 4; $i++) {
+            $id = '11111111-1111-1111-1111-1000000000' . sprintf('%02d', 1 + $i);
+            $user = User::factory()->create([
+                'id' => $id,
+                'email' => "admin.unit{$i}@university.ac.id"
+            ]);
             $unitName = 'Biro Administrasi ' . $i;
             UserProfile::create([
                 'user_id' => $user->id,
                 'identifier_number' => "UNT00{$i}",
-                'full_name' => "Admin Unit {$i}",
+                'full_name' => "Admin Unit $i",
                 'unit_id' => $units[$unitName]->id ?? null,
             ]);
             if (isset($roles['admin_unit'])) $user->roles()->attach($roles['admin_unit']->id);
         }
 
-        // 3. Dosen (5 Users)
-        for ($i = 1; $i <= 5; $i++) {
-            $user = User::factory()->create(['email' => "dosen{$i}@university.ac.id"]);
+        // 3. Dosen (10 Users)
+        for ($i = 1; $i <= 10; $i++) {
+            $id = '11111111-1111-1111-1111-1000000000' . sprintf('%02d', 5 + $i);
+            $user = User::factory()->create([
+                'id' => $id,
+                'email' => "dosen{$i}@university.ac.id"
+            ]);
             UserProfile::create([
                 'user_id' => $user->id,
-                'identifier_number' => "NIDN00{$i}",
-                'full_name' => "Dosen Pengajar {$i}",
+                'identifier_number' => "NIDN" . $faker->numerify('########'),
+                'full_name' => $faker->name,
                 'study_program_id' => $studyPrograms['Teknik Informatika']->id ?? null,
             ]);
             if (isset($roles['dosen'])) $user->roles()->attach($roles['dosen']->id);
         }
 
-        // 4. Mahasiswa (4 Users, 2 di jurusan yang sama)
-        $mahasiswaData = [
-            ['email' => 'mhs1@student.ac.id', 'nim' => 'MHS001', 'name' => 'Mahasiswa 1', 'dept' => 'Sistem Informasi'],
-            ['email' => 'mhs2@student.ac.id', 'nim' => 'MHS002', 'name' => 'Mahasiswa 2', 'dept' => 'Sistem Informasi'], // Jurusan sama
-            ['email' => 'mhs3@student.ac.id', 'nim' => 'MHS003', 'name' => 'Mahasiswa 3', 'dept' => 'Teknik Informatika'],
-            ['email' => 'mhs4@student.ac.id', 'nim' => 'MHS004', 'name' => 'Mahasiswa 4', 'dept' => 'Ilmu Komunikasi'],
-        ];
-
-        foreach ($mahasiswaData as $data) {
-            $user = User::factory()->create(['email' => $data['email']]);
+        // 4. Mahasiswa (5 Users)
+        for ($i = 1; $i <= 5; $i++) {
+            $id = '11111111-1111-1111-1111-1000000000' . sprintf('%02d', 15 + $i);
+            $user = User::factory()->create([
+                'id' => $id,
+                'email' => "mhs{$i}@student.ac.id"
+            ]);
             UserProfile::create([
                 'user_id' => $user->id,
-                'identifier_number' => $data['nim'],
-                'full_name' => $data['name'],
-                'study_program_id' => $studyPrograms[$data['dept']]->id ?? null,
+                'identifier_number' => "MHS" . $faker->numerify('#####'),
+                'full_name' => $faker->name,
+                'study_program_id' => $studyPrograms['Sistem Informasi']->id ?? null,
             ]);
             if (isset($roles['mahasiswa'])) $user->roles()->attach($roles['mahasiswa']->id);
         }
